8570c1f42743254ceedda1b96d6d6891fb2386e4
Bratah123 <brandonnguyen3301@gmail.com>
2024-04-20 17:41:26 -0700

    FEAT: init project

---
+++ start.bat
@@ -0,0 +1,38 @@
+@echo off
+if not exist venv\ (
+    echo No venv folder found
+    echo Setting up virtual environment...
+    :: Generate VENV in project dir
+    python -m venv %~dp0venv
+
+    echo Installing dependencies...
+    :: Activate the VENV
+    call venv\scripts\activate.bat
+    :: Install requirements
+    pip install wheel
+    pip install -r requirements.txt
+)
+
+setlocal
+echo This script will now launch the program.
+echo Please select the environment to run the source code with:
+echo A: Virtual Python Environment (Default)
+echo B: Global Python Environment
+choice /c AB /t 10 /d A /m "What is your choice"
+if errorlevel 2 call :global
+if errorlevel 1 call :virtual
+pause
+endlocal
+
+:: function to run from venv
+:virtual
+echo You have selected A: Virtual Python Environment
+call venv\scripts\activate.bat
+venv\scripts\python main.py
+exit \b
+
+:: function to run from global environment
+:global
+echo You have selected B: Global Python Environment
+python main.py
+exit \b
